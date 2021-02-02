e1dbd121bb1a63b4f645a46cb48ed16a7903ecc3
Muhammed Ahmed Salim <65686246+Ahmed-Salim@users.noreply.github.com>
2021-02-02 12:45:16 +0500

    update delete profile logic

--- php-apis/delete-profile.php
+++ php-apis/delete-profile.php
@@ -11,12 +11,52 @@ if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
     $sql = "DELETE FROM user WHERE id=$user_id";
 
     if (mysqli_query($conn, $sql)) {
-        echo "Your Profile Has Been Deleted Successfully!";
+        echo "Your Profile Has Been Deleted Successfully!<br />";
+
+        $sql2 = "DELETE FROM blocked WHERE block_by=$user_id";
+
+        if (mysqli_query($conn, $sql2)) {
+            echo "User Blocked Data Deleted Successfully<br />";
+        } else {
+            echo "Error deleting user blocked record: " . mysqli_error($conn) . '<br />';
+        }
+
+        $sql3 = "DELETE FROM favourite WHERE fav_by=$user_id";
+
+        if (mysqli_query($conn, $sql3)) {
+            echo "Favourite Data Deleted Successfully<br />";
+        } else {
+            echo "Error deleting favourite record: " . mysqli_error($conn) . '<br />';
+        }
+
+        $sql4 = "DELETE FROM liked WHERE like_by=$user_id";
+
+        if (mysqli_query($conn, $sql4)) {
+            echo "Liked Data Deleted Successfully<br />";
+        } else {
+            echo "Error deleting liked record: " . mysqli_error($conn) . '<br />';
+        }
+
+        $sql5 = "DELETE FROM unliked WHERE unlike_by=$user_id";
+
+        if (mysqli_query($conn, $sql5)) {
+            echo "Unliked Data Deleted Successfully<br />";
+        } else {
+            echo "Error deleting unliked record: " . mysqli_error($conn) . '<br />';
+        }
+
+        $sql5 = "DELETE FROM user_inform WHERE user_id=$user_id";
+
+        if (mysqli_query($conn, $sql5)) {
+            echo "User Inform Data Deleted Successfully<br />";
+        } else {
+            echo "Error deleting user inform record: " . mysqli_error($conn) . '<br />';
+        }
 
         header("Refresh:3; url=./logout.php");
         die();
     } else {
-        echo "Error deleting record: " . mysqli_error($conn);
+        echo "Error deleting profile: " . mysqli_error($conn);
 
         header("Refresh:3; url=../dashboard/profile/");
         die();
